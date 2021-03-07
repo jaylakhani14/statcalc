03f818fb7635430242e19f8b4f1f7fcbf6195d5c
Franklin Tan <fjt7@njit.edu>
2021-03-07 02:06:08 -0500

    Feature: Created test case test_confidence_interval

--- Tests/test_PopulationSample.py
+++ Tests/test_PopulationSample.py
@@ -6,10 +6,20 @@ import scipy
 from PopulationSampling.simpleRandom import Sample
 from PopulationSampling.confidence import Confidence
 
+
 class MyTestCase(unittest.TestCase):
     def test_simple_random_sample(self):
         mylist = ["apple", "banana", "cherry"]
-        Sample.sample(mylist,2)
+        Sample.sample(mylist, 2)
+
+    def test_confidence_interval(self):
+        sample = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+        c = 0.95
+        df = len(sample) - 1
+        sample_mean = np.mean(sample)
+        sample_standard_error = scipy.stats.sem(sample)
+        Confidence.mean_confidence_interval(c, df, sample_mean, sample_standard_error)
+
 
 if __name__ == '__main__':
     unittest.main()
