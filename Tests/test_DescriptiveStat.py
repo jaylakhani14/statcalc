038fc6ef54462732b1196a4d460af767a1dce359
jaylakhani14 <jal98@njit.edu>
2021-03-08 10:19:00 -0500

    Feature : added test_DescriptiveStat.py

---
+++ Tests/test_DescriptiveStat.py
@@ -0,0 +1,41 @@
+import unittest
+from random import random
+
+from DescriptiveStatistics.mean import Mean
+from DescriptiveStatistics.median import Median
+from DescriptiveStatistics.mode import Mode
+from DescriptiveStatistics.variance import Variance
+from DescriptiveStatistics.std import Stddev
+from DescriptiveStatistics.zscore import Zscore
+
+
+
+class MyTestCase(unittest.TestCase):
+
+    def setUp(self) -> None:
+        self.test = [1, 2, 3, 4, 5]
+        self.test1 = [1, 2, 2, 3, 4]
+        self.test2 = [1, 2, 3]
+        self.test3 = [1, 2, 3, 4, 5, 6, 7]
+        self.test4 = [1, 2, 3, 4, 5, 6]
+        self.test5 = [1, 2, 3, 3, 3, 4, 5]
+
+    def test_Mean(self):
+        self.assertEqual(3, Mean.mean(self.test))
+
+    def test_mode(self):
+        self.assertEqual(2, Mode.mode(self.test1))
+
+    def test_median(self):
+        self.assertEqual(2, Median.median(self.test2))
+
+    def test_variance(self):
+        self.assertEqual(2, Variance.variance(self.test))
+
+    def test_stddev(self):
+        self.assertEqual(1.5811388300841898, Stddev.stddev(self.test))
+
+
+
+if __name__ == '__main__':
+    unittest.main()
